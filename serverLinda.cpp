607dcd3d44fcfbb1d74f9210393f250afbd403b1
a761192 <76259300+a761192@users.noreply.github.com>
2020-12-21 20:50:55 +0100

    Add files via upload
    Primera aproximación del código del servidor, usando como base el de la Practica 5.
    Probablemente no compile, las cosas que se me ocurrieron que había que cambiar
    están marcadas con comentarios TODO.

---
+++ serverLinda.cpp
@@ -0,0 +1,137 @@
+//*****************************************************************
+// File:   serverLinda.cpp
+// Author: PSCD-Unizar
+// Date:   diciembre 2020
+// Coms:
+//*****************************************************************
+
+#include <iostream>
+#include <thread>
+#include <string>
+#include <cstdlib>
+#include "Socket.hpp"
+// TODO: #include "PLACEHOLDER_MONITOR"
+// TODO: #include "Tupla.hpp"
+
+using namespace std;
+
+const int N = 5;  // TODO: Clientes simultaneos y/o totales usando el servidor
+
+void servCliente (/* TODO: Socket &soc, int client_fd, Gestor &gestor */) {
+    int length = 100;
+    string buffer = "";
+
+    int rcv_bytes = soc.Recv(client_fd,buffer,length);
+    if (rcv_bytes == -1) {  // TODO: Gestion de errores
+        cerr << "Error al recibir datos: " + string(strerror(errno)) + "\n";
+        // Cerramos los sockets
+        soc.Close(client_fd);
+        exit(1);
+    }
+    if (buffer != "INICIO SESION") {  // TODO: Gestion de errores
+        cerr << "Petición incorrecta: recibido \"" + buffer +
+            "\" cuando se esperaba \"INICIO SESION\"\n";
+        soc.Close(client_fd);
+        exit(1);
+    }
+
+    int send_bytes = soc.Send(client_fd, "SESION ACEPTADA");
+    if(send_bytes == -1) {  // TODO: Gestion de errores
+        cerr << "Error al enviar datos: " + string(strerror(errno)) + "\n";
+        // Cerramos los sockets
+        soc.Close(client_fd);
+        exit(1);
+    }
+
+    bool seguir = true;
+    while (seguir) {
+        rcv_bytes = soc.Recv(client_fd,buffer,length);
+        if (rcv_bytes == -1) {  // TODO: Gestion de errores
+            cerr << "Error al recibir datos: " + string(strerror(errno)) + "\n";
+            // Cerramos los sockets
+            soc.Close(client_fd);
+            exit(1);
+        }
+        if (buffer == "FIN SESION") {  // TODO: Cierre ordinario
+            seguir = false;
+            soc.Close(client_fd);
+        }
+        else {
+            if (buffer == "PN") {
+                // TODO: Recibir parametro, realizar operacion y enviar resultado
+            }
+            else if (buffer == "RN") {
+                // TODO: Recibir parametro, realizar operacion y enviar resultado
+            }
+            else if (buffer == "RdN") {
+                // TODO: Recibir parametro, realizar operacion y enviar resultado
+            }
+            else {  // TODO: Gestion de errores
+                cerr << "Petición incorrecta: recibido \"" + buffer +
+                    "\" cuando se esperaba \"PN\", \"RN\" o \"RdN\"\n";
+                soc.Close(client_fd);
+                exit(1);
+            }
+        }
+    }
+}
+
+int main (int argc, char *argv[]) {
+    // TODO: Asegurar invocacion correcta
+
+    int SERVER_PORT = atoi(argv[1]);
+    thread cliente[N];
+    int client_fd[N];
+    // TODO: PLACEHOLDER_MONITOR monitor();
+
+    // Creación del socket con el que se llevará a cabo
+    // la comunicación con el servidor.
+    Socket chan(SERVER_PORT);
+
+    // Bind
+    int socket_fd = chan.Bind();
+    if (socket_fd == -1) {  // TODO: Gestion de errores
+        cerr << "Error en el bind: " + string(strerror(errno)) + "\n";
+        exit(1);
+    }
+
+    // Listen
+    int error_code = chan.Listen(N);
+    if (error_code == -1) {  // TODO: Gestion de errores
+        cerr << "Error en el listen: " + string(strerror(errno)) + "\n";
+        // Cerramos el socket
+        chan.Close(socket_fd);
+        exit(1);
+    }
+
+    for (int i = 0; i < N; i++) {  // TODO: Cientes a aceptar?
+        // Accept
+        client_fd[i] = chan.Accept();
+
+        if(client_fd[i] == -1) {  // TODO: Gestion de errores
+            cerr << "Error en el accept: " + string(strerror(errno)) + "\n";
+            // Cerramos el socket
+            chan.Close(socket_fd);
+            exit(1);
+        }
+
+        // TODO: cliente[i] = thread(&servCliente, ref(chan), client_fd[i], ref(gestor));
+    }
+
+    // ¿Qué pasa si algún thread acaba inesperadamente?
+    for (int i=0; i<N; i++) {
+        cliente[i].join();
+    }
+
+    // Cerramos el socket del servidor
+    error_code = chan.Close(socket_fd);
+    if (error_code == -1) {  // TODO: Gestion de errores
+        cerr << "Error cerrando el socket del servidor: " + string(strerror(errno)) + " aceptado" + "\n";
+    }
+
+    // TODO: Fin del programa
+    // Despedida
+    cout << "Bye bye" << endl;
+
+    return error_code;
+}
