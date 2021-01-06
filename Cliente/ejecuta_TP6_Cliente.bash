4b206d86c541f0e0660b08f91903c75004b3faba
Albertus7500 <59068447+Albertus7500@users.noreply.github.com>
2021-01-06 21:27:05 +0100

    Add files via upload

---
+++ Cliente/ejecuta_TP6_Cliente.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+#*****************************************************************
+# File:   ejecuta_p5_SERVER.bash
+# Author: Alberto Carmona estradera
+# Date:   Enero 2021
+# Coms:   script de puesta en marcha del servidor
+#        
+#*****************************************************************
+
+MAKE_FILE=Makefile_TP6_Cliente
+EJECUTABLE=Cliente
+
+IPDESPLIEGUE=localhost
+PUERTODESPLIEGUE=50000
+
+
+echo "------------------------------------------"
+echo "compliando cliente..."
+make -f $MAKE_FILE
+resMake=$?
+if [[ "${resMake}" == 0 ]]
+	then    #ha funcionado bien
+		echo "${FORM}Compila correctamente"
+	else
+		echo "${FORM}ERROR: fallo al compilar"
+		exit 0
+	fi
+echo "ejecutando cliente..."
+./$EJECUTABLE  $IPDESPLIEGUE $PUERTODESPLIEGUE
+exit 0
