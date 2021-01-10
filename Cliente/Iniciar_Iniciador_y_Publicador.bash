dc1bb1613bafb98d379e6feeb76dee3fa228d144
Albertus7500 <59068447+Albertus7500@users.noreply.github.com>
2021-01-10 20:25:33 +0100

    Add files via upload

---
+++ Cliente/Iniciar_Iniciador_y_Publicador.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+#*****************************************************************
+# File:   ejecuta_p5_SERVER.bash
+# Author: Alberto Carmona estradera
+# Date:   Enero 2021
+# Coms:   script de puesta en marcha del servidor
+#        
+#*****************************************************************
+
+MAKE_FILE=Makefile_TP6_Cliente
+PUBLICADOR=publicador
+INICIADOR=iniciador
+IPDESPLIEGUE=localhost
+PUERTODESPLIEGUE=2200
+
+
+
+echo "------------------------------------------"
+echo "compliando cliente..."
+make -f $MAKE_FILE
+resMake=$?
+if [[ "${resMake}" == 0 ]]
+	then    #ha funcionado bien
+		echo "${FORM}Compila correctamente"
+	else
+		echo "${FORM}ERROR: fallo al compilar"
+		exit 0
+	fi
+echo "ejecutando cliente..."
+./$INICIADOR $IPDESPLIEGUE $PUERTODESPLIEGUE
+./$PUBLICADOR  $IPDESPLIEGUE $PUERTODESPLIEGUE
+
+exit 0
